f95530802901faaebd04cbf8f63bea43b2e6d03c
dukim <sajacall82@naver.com>
2023-09-18 14:38:06 +0900

    feat: print fizz
    if num is multiple of 3, print fizz
    otherwise, print num

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,16 @@
-print('hello')
+def fizzbuzz(num: int):
+    """
+    if num is multiple of 3, print fizz
+    otherwise print num
+    :param num: loop count
+    :return:  None
+    """
+    for n in range(1, num+1):
+        if n%3==0:
+            print('fizz')
+        else:
+            print(n)
+
+
+if __name__ == '__main__':
+    fizzbuzz(15)
